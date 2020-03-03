a2ba2a076dd736ce213d27c40b3202c766455142
modupe <adewalebose15@gmail.com>
2020-03-03 08:35:13 +0100

    Addes bubble_sort_by method

--- Bubble_sort.rb
+++ Bubble_sort.rb
@@ -16,3 +16,28 @@ def bubble_sort(arr)
     end
 
  bubble_sort([4,3,78,2,0,2])
+
+ def bubble_sort_by(arr)
+  i = 0
+  while i < arr.length-1 
+    if yield(arr[i],arr[i + 1])
+      arr[i],arr[i + 1] = arr[i + 1],arr[i] if arr[i] > arr[i + 1]
+     if i == arr.length-2
+      if arr != arr.sort
+        i = 0
+        next
+      else
+        break
+      end
+     end
+    i += 1
+    end
+  end
+  puts "#{arr}"
+end
+
+ 
+
+ bubble_sort_by(["hi", "hello", "hey"]) do |first, second|
+first.length <=> second.length
+ end
