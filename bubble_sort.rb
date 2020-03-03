b6a180f76e30b9f61e9cd4517a932198d268373b
modupe <adewalebose15@gmail.com>
2020-03-03 14:57:48 +0100

    Fixed rubocop issue for first method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,38 +2,35 @@ def bubble_sort(arr)
   i = 0
   while i < arr.length - 1
     arr[i], arr[i + 1] = arr[i + 1], arr[i] if arr[i] > arr[i + 1]
-    if i == arr.length - 2      
-      unless arr != arr.sort
-        break
-      else
-        i = 0
-        next
-      end      
-    end  
-  i += 1
+    if i == arr.length - 2
+      break unless arr != arr.sort
+
+      i = 0
+      next
+    end
+    i += 1
   end
   puts arr.to_s
 end
-
 
 bubble_sort([4, 3, 78, 2, 0, 2])
 
 def bubble_sort_by(arr)
   i = 0
   while i < arr.length - 1
-    if yield(arr[i], arr[i + 1])
+    next unless yield(arr[i], arr[i + 1])
 
     arr[i], arr[i + 1] = arr[i + 1], arr[i] if arr[i].length > arr[i + 1].length
     if i == arr.length - 2
       break unless arr != arr.sort { |x, y| x.length <=> y.length }
-        i = 0      
+
+      i = 0
     end
     i += 1
   end
-end
   puts arr.to_s
 end
 
 bubble_sort_by(%w[hello hi hey]) do |first, second|
-  first.length <=> second.length  
+  first.length <=> second.length
 end
