2b6b7f807abb6d25a90201c9754be4573cfeeedc
modupe <adewalebose15@gmail.com>
2020-03-03 11:31:09 +0100

    Fixed the yield error on bubble sort method

--- Bubble_sort.rb
+++ Bubble_sort.rb
@@ -22,40 +22,24 @@ def bubble_sort(arr)
  def bubble_sort_by(arr)
   i = 0
   while i < arr.length-1 
-<<<<<<< HEAD
     if yield(arr[i],arr[i + 1])
-      arr[i],arr[i + 1] = arr[i + 1],arr[i] if arr[i] > arr[i + 1]
+      arr[i],arr[i + 1] = arr[i + 1],arr[i]  if arr[i].length > arr[i + 1].length
        if i == arr.length-2
-        if arr != arr.sort
+        if arr != arr.sort {|x, y| x.length <=> y.length}
           i = 0
           next
         else
           break
         end
        end
-=======
-      yield(arr[i],arr[i + 1], i)
-      puts "It works"
-      puts " #{i} #{arr[i]} = #{arr[i].size} #{arr[i + 1]} = #{arr[i + 1].size}"
-      #arr[i],arr[i + 1] = arr[i + 1],arr[i] if arr[i] > arr[i + 1]      
-     if i == arr.length-2
-      if arr != arr.sort {|x, y| x.length <=> y.length}
-        i = 0
-        next
-      else
-        break
-      end
-     end
->>>>>>> 7258d90e7f48cccb8dd898169745304917b36b09
     i += 1
     end
-  #end
+  end
   puts "#{arr}"
 end
 
-#puts "#{arr.sort}"
 
- bubble_sort_by(["hi", "adsgsg", "hello", "hey"]) do |first, second, i|  
+ bubble_sort_by(["hello", "hi", "hey"]) do |first, second|  
   first.length <=> second.length 
-  first,second = second, first if first > second
+  # puts "#{first} #{second}"
  end
